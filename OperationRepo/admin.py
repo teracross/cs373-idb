3606b785c0e88665be933151b713022391b5e1a7
Po-Chen Yang <pcyang@cs.utexas.edu>
2014-04-01 20:52:21 -0500

    added models into admin

--- OperationRepo/admin.py
+++ OperationRepo/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from OperationRepo.models import *
+
+admin.site.register(Business)
+admin.site.register(Neighborhoods)
+admin.site.register(Categories)
+admin.site.register(Attritubes)
+admin.site.register(Hours)
+admin.site.register(User)
+admin.site.register(User_Votes)
+admin.site.register(Friends)
+admin.site.register(Elite)
+admin.site.register(Compliments)
+admin.site.register(Review)
+admin.site.register(Review_Votes)
