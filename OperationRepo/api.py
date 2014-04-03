a09c9cc62fb011aac04763904b55efb19e71c651
Will Nation <wnation@cs.utexas.edu>
2014-04-03 13:13:34 -0500

    Remove meta and resource_uri from response

--- OperationRepo/api.py
+++ OperationRepo/api.py
@@ -3,14 +3,21 @@ from tastypie import fields
 from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from OperationRepo.models import *
+import copy
 
 class BusinessResource(ModelResource):
 
 	class Meta:
 		queryset = Business.objects.all()
 		resource_name = 'business'
+		include_resource_uri = False
 		authorization= Authorization()
-		excludes = ["resource_uri"]
+
+	def alter_list_data_to_serialize(self, request, business_dict):
+		if isinstance(business_dict, dict):
+			if 'meta' in business_dict:
+				del(business_dict['meta'])
+		return business_dict['objects']
 
 class NeighborhoodsResource(ModelResource):
 	business = fields.ForeignKey(BusinessResource, 'business')
@@ -45,7 +52,14 @@ class UserResource(ModelResource):
 	class Meta:
 		queryset = User.objects.all()
 		resource_name = 'user'
+		include_resource_uri = False
 		authorization= Authorization()
+
+	def alter_list_data_to_serialize(self, request, user_dict):
+		if isinstance(user_dict, dict):
+			if 'meta' in user_dict:
+				del(user_dict['meta'])
+		return user_dict['objects']
 
 class User_VotesResource(ModelResource):
 	user = fields.ForeignKey(UserResource, 'user')
@@ -69,14 +83,21 @@ class ComplimentsResource(ModelResource):
 		resource_name = 'compliments'
 
 class ReviewResource(ModelResource):
-	business = fields.ForeignKey(BusinessResource, 'business')
-	user = fields.ForeignKey(UserResource, 'user')
+	business_id = fields.ForeignKey(BusinessResource, 'business_id')
+	user_id = fields.ForeignKey(UserResource, 'user_id')
 
 
 	class Meta:
 		queryset = Review.objects.all()
 		resource_name = 'review'
+		include_resource_uri = False
 		authorization= Authorization()
+
+	def alter_list_data_to_serialize(self, request, review_dict):
+		if isinstance(review_dict, dict):
+			if 'meta' in review_dict:
+				del(review_dict['meta'])
+		return review_dict['objects']
 
 class Review_VotesResource(ModelResource):
 	review = fields.ForeignKey(ReviewResource, 'review')
