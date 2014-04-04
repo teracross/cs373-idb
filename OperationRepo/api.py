c1eb1e63ab44a16f70b05a932803a4e54f25d693
Po-Chen Yang <pcyang@cs.utexas.edu>
2014-04-04 01:52:19 -0500

    fix json format

--- OperationRepo/api.py
+++ OperationRepo/api.py
@@ -20,8 +20,12 @@ from django.core.serializers.json import DjangoJSONEncoder
 # Businesses
 def get_business_all(request):
     return HttpResponse(json.dumps(list(Business.objects.all().values('business_id','name'))))
+
 def get_business_id(request, business_id):
-    return HttpResponse(list(Business.objects.filter(business_id=business_id).values()))
+    business = get_object_or_404(Business, business_id=business_id)
+    business.__dict__.pop('_state')
+    return HttpResponse(json.dumps(business.__dict__))
+
 def get_business_id_user(request, business_id):
     business = get_object_or_404(Business, business_id=business_id)
     reviews = Review.objects.filter(business=business)
@@ -30,6 +34,7 @@ def get_business_id_user(request, business_id):
         review.user.__dict__['yelping_since'] = str(review.user.__dict__['yelping_since'])
     users = [review.user.__dict__ for review in reviews]
     return HttpResponse(json.dumps(users))
+
 def get_business_id_review(request, business_id):
     business = get_object_or_404(Business, business_id=business_id)
     return HttpResponse(json.dumps(list(Review.objects.filter(business=business).values()),cls=DjangoJSONEncoder))
@@ -37,8 +42,13 @@ def get_business_id_review(request, business_id):
 # Users
 def get_user_all(request):
     return HttpResponse(json.dumps(list(User.objects.all().values('user_id','name'))))
+
 def get_user_id(request, user_id):
-    return HttpResponse(list(User.objects.filter(user_id=user_id).values()))
+    user = get_object_or_404(User, user_id=user_id)
+    user.__dict__.pop('_state')
+    user.__dict__['yelping_since'] = str(user.__dict__['yelping_since'])
+    return HttpResponse(json.dumps(user.__dict__))
+
 def get_user_id_business(request, user_id):
     user = get_object_or_404(User, user_id=user_id)
     reviews = Review.objects.filter(user=user)
@@ -54,8 +64,12 @@ def get_user_id_review(request, user_id):
 def get_review_all(request):
     result =[{'user_id':review['user'], 'review_id':review['review_id'], 'business_id':review['business']} for review in Review.objects.all().values('review_id','user','business')]
     return HttpResponse(json.dumps(result))
+
 def get_review_id(request, review_id):
-    return HttpResponse(list(Review.objects.filter(review_id=review_id).values()))
+    review = get_object_or_404(Review, review_id=review_id)
+    review.__dict__.pop('_state')
+    review.__dict__['date'] = str(review.__dict__['date'])
+    return HttpResponse(json.dumps(review.__dict__))
 
 def get_review_id_business(request, review_id):
     review = get_object_or_404(Review, review_id=review_id)
