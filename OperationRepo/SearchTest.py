20e7799e31c4a105e480ace892c36cf78779aa27
aag2423 <anthony.garza@utexas.edu>
2014-04-16 18:51:23 -0500

    added searchtest.py for unit tests

---
+++ OperationRepo/SearchTest.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from json import dumps, loads
+from django.test import TestCase
+
+class SearchTest(TestCase):
+	pass
+
+
+print("Done")
