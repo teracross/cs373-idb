35938e819870bf474cc4ce83ce85300444290dc4
aag2423 <anthony.garza@utexas.edu>
2014-04-16 17:56:52 -0500

    changed settings

--- idb/settings.py
+++ idb/settings.py
@@ -57,8 +57,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'OperationRepo',
-    'tastypie'
+    'OperationRepo'
 )
 
 MIDDLEWARE_CLASSES = (
